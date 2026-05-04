7a1e6e0c75a8d406f838c60ed17f7326b201f789
Matias Oyhamburu <149548441+Mattew0800@users.noreply.github.com>
2026-05-04 16:37:54 -0300

    feat(hj-1): continue button and images (ball and background separately)

--- WEB/src/app/app.routes.ts
+++ WEB/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import {LogIn} from './screens/log-in/log-in';
 import {Onboarding} from './screens/onboarding/onboarding';
 import {SignUp} from './screens/sign-up/sign-up';
 import {Error} from './screens/error/error';
+import {SplashComponent} from './screens/splash/splash';
 
 export const routes: Routes = [
-  {path:'', component:Onboarding},
+  {path:'', component:SplashComponent},
+  {path:'onboarding', component:Onboarding},
   {path:'sign-in', component:LogIn},
   {path:'sign-up', component:SignUp},
   {path:'**', component:Error},
--- WEB/src/app/screens/onboarding/onboarding.ts
+++ WEB/src/app/screens/onboarding/onboarding.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit, OnDestroy, Inject, Renderer2 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-import {RouterLink} from '@angular/router';
 import { Meta } from '@angular/platform-browser';
 import { SvgLoaderComponent } from '../../components/svg-loader/svg-loader.component';
 
 @Component({
   selector: 'app-onboarding',
-  imports: [RouterLink, SvgLoaderComponent],
+  imports: [SvgLoaderComponent],
   templateUrl: './onboarding.html',
   styleUrl: './onboarding.scss',
 })
 export class Onboarding implements OnInit, OnDestroy {
-  window=window;
-  currentStep: 'splash' | 'onboarding' = 'splash';
+  window = window;
 
   constructor(
     private meta: Meta,
@@ -21,25 +19,12 @@ export class Onboarding implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.meta.updateTag({ name: 'theme-color', content: '#000000' });
-    this.renderer.setStyle(this.document.body, 'background-color', '#000000');
-    // Para cuando uses una imagen de fondo en el body, podrías hacer:
-    // this.renderer.setStyle(this.document.body, 'background-image', "url('...')");
-
-    // Mostrar splash (logo) durante 2 segundos
-    setTimeout(() => {
-      this.currentStep = 'onboarding';
-      this.meta.updateTag({ name: 'theme-color', content: '#181b16' });
-      this.renderer.setStyle(this.document.body, 'background-color', '#181b16');
-      // this.renderer.setStyle(this.document.body, 'background-image', "url('tu-imagen-aqui.jpg')");
-      // this.renderer.setStyle(this.document.body, 'background-size', 'cover');
-    }, 2000);
+    this.meta.updateTag({ name: 'theme-color', content: '#181b16' });
+    this.renderer.setStyle(this.document.body, 'background-color', '#181b16');
   }
 
   ngOnDestroy() {
     this.meta.updateTag({ name: 'theme-color', content: '#000000' });
     this.renderer.removeStyle(this.document.body, 'background-color');
-    this.renderer.removeStyle(this.document.body, 'background-image');
-    this.renderer.removeStyle(this.document.body, 'background-size');
   }
 }
