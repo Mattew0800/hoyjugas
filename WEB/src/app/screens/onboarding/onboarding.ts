9462a50941577cb7107544673750959c4536dc36
Matias Oyhamburu <149548441+Mattew0800@users.noreply.github.com>
2026-06-04 15:04:26 -0300

    fix(hj-1): fix onboarding fonts, delete src (spring) duplicated, add audio-service for future implementation in onboarding.

--- WEB/src/app/screens/onboarding/onboarding.ts
+++ WEB/src/app/screens/onboarding/onboarding.ts
@@ -3,6 +3,7 @@ import { DOCUMENT } from '@angular/common';
 import { Meta } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { SvgLoaderComponent } from '../../components/svg-loader/svg-loader.component';
+import { AudioService } from '../../services/AudioService/audio-service';
 
 @Component({
   selector: 'app-onboarding',
@@ -18,22 +19,30 @@ export class Onboarding implements OnInit, OnDestroy {
     private meta: Meta,
     @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2,
-    private router: Router
+    private router: Router,
+    private audioService: AudioService
   ) {}
 
   ngOnInit() {
     this.meta.updateTag({ name: 'theme-color', content: '#181b16' });
     this.renderer.setStyle(this.document.body, 'background-color', '#CEA764');
+    this.audioService.loadAudio('kick', '/assets/audio/kick.mp3');
   }
 
   ngOnDestroy() {
     this.meta.updateTag({ name: 'theme-color', content: '#000000' });
     this.renderer.removeStyle(this.document.body, 'background-color');
+    this.audioService.unload('kick');
   }
 
   onContinueClick() {
-    if (this.ballImage) {
+    /*
+    this.audioService.play('kick', 0.4).catch(err => {
+      console.warn('No se pudo reproducir el audio:', err);
+    });
+    */
 
+    if (this.ballImage) {
       this.renderer.addClass(
         this.ballImage.nativeElement,
         'shoot'
@@ -42,7 +51,6 @@ export class Onboarding implements OnInit, OnDestroy {
       setTimeout(() => {
         this.router.navigate(['/sign-in']);
       }, 1500);
-
 
     }
   }
