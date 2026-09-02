de9a22572414a4f943bfdbc7058ff483aa606c1d
Matias Oyhamburu <149548441+Mattew0800@users.noreply.github.com>
2026-09-02 20:41:22 -0300

    feature(): create booking success component (multi step process)

---
+++ WEB/src/app/screens/booking-success/booking-success.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-success',
+  imports: [],
+  templateUrl: './booking-success.html',
+  styleUrl: './booking-success.scss',
+})
+export class BookingSuccess {
+
+}
