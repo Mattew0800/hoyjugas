243fcd0e73892478703ab9bac6024e2b79210969
Nachoerbin <erbinignacio@gmail.com>
2026-09-12 16:11:54 -0300

    feat(hj-1): add system configuration screen

--- WEB/src/app/app.routes.ts
+++ WEB/src/app/app.routes.ts
@@ -20,6 +20,7 @@ import {authGuard} from './auth/AuthGuard';
 import {guestGuard} from './auth/GuestGuard';
 import {internalAuthGuard} from './auth/InternalAuthGuard';
 import {CustomersScreen} from './screens/internal/Customers/customers-screen/customers-screen';
+import {SystemConfigScreen} from './screens/internal/SystemConfig/system-config/system-config';
 
 export const routes: Routes = [
   {path:'', component:SplashComponent},
@@ -39,6 +40,7 @@ export const routes: Routes = [
   {path:'internal/spaces',component: SpacesScreen, canActivate:[internalAuthGuard]},
   {path:'internal/employees', component:EmployeesScreen, canActivate:[internalAuthGuard]},
   {path:'internal/customers', component:CustomersScreen, canActivate:[internalAuthGuard]},
+  {path:'internal/config', component:SystemConfigScreen, canActivate:[internalAuthGuard]},
   {path:'**', component:Error}
 
 ];
